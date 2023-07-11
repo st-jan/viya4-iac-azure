66426885e6d57278b47957e7fe0b45b94df889b1
Jan Stienstra <Jan.Stienstra@sas.com>
2023-07-11 10:29:00 +0200

    Add tags to additional resources

--- modules/azurerm_acr/main.tf
+++ modules/azurerm_acr/main.tf
@@ -39,6 +39,7 @@ resource "azurerm_private_dns_zone" "acr" {
 
   name                = "privatelink.azurecr.io"
   resource_group_name = var.network_resource_group_name
+  tags                = var.tags
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "acr" {
@@ -48,6 +49,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "acr" {
   private_dns_zone_name = azurerm_private_dns_zone.acr[0].name
   virtual_network_id    = var.virtual_network_id
   resource_group_name   = var.network_resource_group_name
+  tags                  = var.tags
 }
 
 resource "azurerm_private_endpoint" "acr" {
